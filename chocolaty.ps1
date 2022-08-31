906ee92d30b338748b7702ec96f6e8a003809518
Daniel Fischer <daniel.fischer@octopus.com>
2022-08-31 13:30:59 +1000

    Update chocolaty.ps1

--- chocolaty.ps1
+++ chocolaty.ps1
@@ -6,12 +6,12 @@ Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://cho
 choco install notepadplusplus -y
 
 # Configure server as both client and server to enable double hops (https://octopus.com/blog/azure-script-extension#supporting-powershell-double-hops)
-#Enable-WSManCredSSP -Role Server -Force
-#Enable-WSManCredSSP -Role Client -DelegateComputer * -Force
+Enable-WSManCredSSP -Role Server -Force
+Enable-WSManCredSSP -Role Client -DelegateComputer * -Force
 
 # Allow use of NTLM account since we're using local account and not domain.
-#New-Item -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation -Name AllowFreshCredentialsWhenNTLMOnly -Force
-#New-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation\AllowFreshCredentialsWhenNTLMOnly -Name 1 -Value * -PropertyType String
+New-Item -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation -Name AllowFreshCredentialsWhenNTLMOnly -Force
+New-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation\AllowFreshCredentialsWhenNTLMOnly -Name 1 -Value * -PropertyType String
 
 
 # Run PowerShell script under a new user & Install SQL-Server-Express
