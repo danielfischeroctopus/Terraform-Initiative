15221ea2992aa4e4d5047da3666173955f29cca2
Daniel Fischer <daniel.fischer@octopus.com>
2022-09-02 16:19:56 +1000

    Update TeamCityConfig.ps1

--- TeamCityConfig.ps1
+++ TeamCityConfig.ps1
@@ -1,1 +1,17 @@
+####################################################################################################
+# TeamCity server configuration script.
+# 
+# Install Choco, TeamCity
+# 
+# 
+# 
+####################################################################################################
 
+
+# Set Execution Policy
+Set-ExecutionPolicy Bypass -Scope Process -Force
+
+# Install Chocolaty
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+choco install teamcity
