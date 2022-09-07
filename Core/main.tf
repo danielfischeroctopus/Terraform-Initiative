a0fcb93273c167653d41687a23a88cf47362e96f
Daniel Fischer <daniel.fischer@octopus.com>
2022-09-07 14:27:59 +1000

    Update main.tf

--- Core/main.tf
+++ Core/main.tf
@@ -1,1 +1,88 @@
+## Locals for DC promotion.
+locals {
+  # Create DC commands
 
+  winrm_command        = "winrm quickconfig -quiet"
+  import_command       = "Import-Module ADDSDeployment"
+  password_command     = "$password = ConvertTo-SecureString ${var.domainPass} -AsPlainText -Force"
+  install_ad_command   = "Add-WindowsFeature -name ad-domain-services -IncludeManagementTools"
+  configure_ad_command = "Install-ADDSForest -CreateDnsDelegation:$false -DomainMode 7 -DomainName ${var.active_directory_domain} -ForestMode 7 -InstallDns:$true -SafeModeAdministratorPassword $password -Force:$true"
+  shutdown_command     = "shutdown -r -t 10"
+  exit_code_hack       = "exit 0"
+  powershell_command   = "${local.winrm_command}; ${local.import_command}; ${local.password_command}; ${local.install_ad_command}; ${local.configure_ad_command}; ${local.shutdown_command}; ${local.exit_code_hack}"
+}
+
+# Resources and Providers
+
+provider "azurerm" {
+  features {}
+}
+
+provider "ad" {
+  winrm_hostname = var.hostname
+  winrm_username = var.domainUser
+  winrm_password = var.domainPass
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.rgName
+  location = var.location
+}
+
+# Networking module
+
+module "networking" {
+  source = ".\\modules\\networking"
+
+  rgName     = var.rgName
+  location   = var.location
+  virNetName = var.virNetName
+  subnetName = var.subnetName
+
+  depends_on = [azurerm_resource_group.rg]
+}
+
+# VM Module
+
+module "DC_vm" {
+  source = ".\\modules\\vm"
+
+  vmName     = "DC"
+  subnetId   = module.networking.subnet_id
+  rgName     = var.rgName
+  location   = var.location
+  localUser  = var.localUser
+  localPass  = var.localPass
+  domainUser = var.domainUser
+  domainPass = var.domainPass
+  extension  = <<SETTINGS
+    {
+        "commandToExecute": "powershell.exe -Command \"${local.powershell_command}\""
+    }
+SETTINGS
+
+  depends_on = [
+    azurerm_resource_group.rg,
+    module.networking
+  ]
+}
+
+module "SQL_vm" {
+  source = ".\\modules\\vm"
+
+  vmName     = "SQL"
+  subnetId   = module.networking.subnet_id
+  rgName     = var.rgName
+  location   = var.location
+  localUser  = var.localUser
+  localPass  = var.localPass
+  domainUser = var.domainUser
+  domainPass = var.domainPass
+  extension  = var.extSQL
+
+  depends_on = [
+    azurerm_resource_group.rg,
+    module.networking,
+    module.DC_vm
+  ]
+}
