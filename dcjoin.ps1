492f0e4ae43bfb54b1feeb3ead374b41ab1b9716
Daniel Fischer <daniel.fischer@octopus.com>
2022-09-08 15:52:44 +1000

    Update dcjoin.ps1

--- dcjoin.ps1
+++ dcjoin.ps1
@@ -1,5 +1,5 @@
 $dc = "TFI.LOCAL"
 $pw = "Passw0rd1" | ConvertTo-SecureString -asPlainText –Force
-$usr = "TFI.LOCAL\daniel"
+$usr = 'TFI.LOCAL\daniel'
 $creds = New-Object System.Management.Automation.PSCredential($usr,$pw)
 Add-Computer -DomainName $dc -Credential $creds -restart -force -verbose
