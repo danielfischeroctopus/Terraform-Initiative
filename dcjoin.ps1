08e1971fbe1caad344854ecfd7b5b5f31de33723
Daniel Fischer <daniel.fischer@octopus.com>
2022-09-09 15:17:48 +1000

    Delete dcjoin.ps1

--- dcjoin.ps1
+++
@@ -1,7 +0,0 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force
-
-$dc = 'TFI.LOCAL'
-$pw = 'Passw0rd1' | ConvertTo-SecureString -asPlainText –Force
-$usr = 'TFI.LOCAL\daniel'
-$creds = New-Object System.Management.Automation.PSCredential($usr,$pw)
-Add-Computer -DomainName $dc -Credential $creds -restart -force -verbose
