c3be75aa821fff04d2cf445f52c3efe75c9da032
Daniel Fischer <daniel.fischer@octopus.com>
2022-09-16 12:24:37 +1000

    Update DCConfig.ps1

--- DCConfig.ps1
+++ DCConfig.ps1
@@ -1,6 +1,6 @@
 New-SelfSignedCertificate -Subject 'CN=DC.TFI.LOCAL' -TextExtension '2.5.29.37={text}1.3.6.1.5.5.7.3.1'
 $thumbprint = (Get-ChildItem -Path 'Cert:LocalMachine\MY' | Where-Object {$_.Subject -Match 'CN=DC.TFI.LOCAL'}).Thumbprint
-winrm quickconfig -quiet
+
 winrm create winrm/config/Listener?Address=*+Transport=HTTPS '@{Hostname=${DC.TFI.LOCAL}; CertificateThumbprint=$thumpprint}'
 
 $FirewallParam = @{
