d55a7b991968b61b507c77c6d47eb01a0b30e41c
Daniel Fischer <daniel.fischer@octopus.com>
2022-10-06 09:37:51 +1000

    Update DCConfig.ps1

--- DCConfig.ps1
+++ DCConfig.ps1
@@ -2,6 +2,15 @@ Set-ExecutionPolicy Bypass -Scope Process -Force
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install golang
 
+winrm quickconfig -quiet
+Import-Module ADDSDeployment
+Import-Module ActiveDirectory
+$password = ConvertTo-SecureString Passw0rd1 -AsPlainText -Force
+Add-WindowsFeature -name ad-domain-services -IncludeManagementTools
+Install-ADDSForest -CreateDnsDelegation:$false -DomainMode 7 -DomainName "TFI.LOCAL" -ForestMode 7 -InstallDns:$true -SafeModeAdministratorPassword $password -Force:$true
+shutdown -r -t 10
+
+
 /*
 New-SelfSignedCertificate -Subject 'CN=DC.TFI.LOCAL' -TextExtension '2.5.29.37={text}1.3.6.1.5.5.7.3.1'
 $thumbprint = (Get-ChildItem -Path 'Cert:LocalMachine\MY' | Where-Object {$_.Subject -Match 'CN=DC.TFI.LOCAL'}).Thumbprint
