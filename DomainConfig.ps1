add4e7e7b94e670daa1415ecdbf227b41742ab1b
Daniel Fischer <daniel.fischer@octopus.com>
2022-10-13 16:00:13 +1000

    Update DomainConfig.ps1

--- DomainConfig.ps1
+++ DomainConfig.ps1
@@ -6,8 +6,8 @@ Set-Item WSMan:\localhost\Client\TrustedHosts * -Force
 #Install-Module PowerShellGet -AllowClobber -Force
 #Install-PackageProvider NuGet -Force
 Import-Module ActiveDirectory -Force
-Set-ExecutionPolicy Bypass -Scope Process -Force
-Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+#Set-ExecutionPolicy Bypass -Scope Process -Force
+#Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 $pass = ConvertTo-SecureString $pass -AsPlainText -Force
 Add-WindowsFeature -name ad-domain-services -IncludeManagementTools
 Install-ADDSForest -CreateDnsDelegation:$false -DomainMode 7 -DomainName "TFI.LOCAL" -ForestMode 7 -InstallDns:$true -SafeModeAdministratorPassword $pass -Force:$true
