533e99265c68ac52ecd4100bdac8e51e1104befd
Daniel Fischer <daniel.fischer@octopus.com>
2022-12-02 12:46:09 +1000

    Update DomainConfig.ps1

--- DomainConfig.ps1
+++ DomainConfig.ps1
@@ -5,6 +5,9 @@ winrm quickconfig -quiet
 Set-Item WSMan:\localhost\Client\TrustedHosts * -Force
 #Install-Module PowerShellGet -AllowClobber -Force
 #Install-PackageProvider NuGet -Force
+winrm set winrm/config/service/Auth '@{Basic="true"}'
+winrm set winrm/config/service '@{AllowUnencrypted="true"}'
+winrm set winrm/config/winrs '@{MaxMemoryPerShellMB="1024"}'
 
 Import-Module ActiveDirectory -Force
 #Set-ExecutionPolicy Bypass -Scope Process -Force
