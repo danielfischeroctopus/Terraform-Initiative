700f40f1e0dada7438bd344bc58f0269da0dafce
Daniel Fischer <daniel.fischer@octopus.com>
2023-01-20 09:14:26 +1000

    Update DomainConfig.ps1

--- DomainConfig.ps1
+++ DomainConfig.ps1
@@ -3,8 +3,7 @@ $pass = "Passw0rd1"
 $pass = ConvertTo-SecureString $pass -AsPlainText -Force
 
 Add-WindowsFeature -name ad-domain-services -IncludeManagementTools
-Install-ADDSForest -CreateDnsDelegation:$false -DomainMode 7 -DomainName "TFI.LOCAL" -ForestMode 7 -InstallDns:$true -SafeModeAdministratorPassword $pass -Force:$true
-Start-Sleep -s 10
+Install-ADDSForest -CreateDnsDelegation:$false -DomainMode 7 -DomainName "TFI.LOCAL" -ForestMode 7 -InstallDns:$true -SafeModeAdministratorPassword $pass -NoRebootOnCompletion -Force:$true
 Start-Service -Name "Active Directory Domain Services"
 Start-Sleep -s 10
 New-AdOrganizationalUnit -Name "Domain Computers" -Path "DC=TFI,DC=LOCAL"  -ProtectedFromAccidentalDeletion $False
