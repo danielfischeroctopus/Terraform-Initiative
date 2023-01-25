3a9fc80c85de1f2d0da178c8403f0995f9b72462
Daniel Fischer <daniel.fischer@octopus.com>
2023-01-25 10:55:00 +1000

    Update DomainConfig.ps1

--- DomainConfig.ps1
+++ DomainConfig.ps1
@@ -5,7 +5,7 @@ $pass = ConvertTo-SecureString $pass -AsPlainText -Force
 Add-WindowsFeature -name ad-domain-services -IncludeManagementTools
 Install-ADDSForest -CreateDnsDelegation:$false -DomainMode 7 -DomainName "TFI.LOCAL" -ForestMode 7 -InstallDns:$true -SafeModeAdministratorPassword $pass -NoRebootOnCompletion -Force:$true
 
-Start-Transcript -Path "C:\transcripts\transcript0.txt" -NoClobber
+<#Start-Transcript -Path "C:\transcripts\transcript0.txt" -NoClobber
 
 Start-Sleep -s 10
 Start-Service -DisplayName "Active Directory Web Services"
@@ -14,6 +14,6 @@ Start-Service -DisplayName "Active Directory Domain Services"
 Start-Sleep -s 10 # Gives time for the ADWS service to boot and accept the next command.
 New-AdOrganizationalUnit -Name "Domain Computers" -Path "DC=TFI,DC=LOCAL" -ProtectedFromAccidentalDeletion $False
 
-Stop-Transcript
+Stop-Transcript#>
 
 shutdown -r -t 10
