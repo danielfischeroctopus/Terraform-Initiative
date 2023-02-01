382fce7079c869fca1eebffdf7ce9ec5b69291f0
Daniel Fischer <daniel.fischer@octopus.com>
2023-02-01 12:56:06 +1000

    Update SQLConfig.ps1

--- SQLConfig.ps1
+++ SQLConfig.ps1
@@ -6,8 +6,6 @@
 # 
 # 
 ####################################################################################################
-Write-Host "Starting Sleep for 60 seconds."
-Start-Sleep -s 60
 
 Start-Transcript -Path "C:\transcript.txt" -NoClobber
 
@@ -15,7 +13,7 @@ $name = "SQL"
 $dcname = "10.0.1.10"
 
 # Install Chocolaty
-#Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # Configure server as both client and server to enable double hops (https://octopus.com/blog/azure-script-extension#supporting-powershell-double-hops)
 Enable-WSManCredSSP -Role Server -Force
@@ -30,34 +28,7 @@ New-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDele
 $securePassword = ConvertTo-SecureString 'Passw0rd1' -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential 'TFI.LOCAL\daniel', $securePassword
 
-$status = 0
-while ($status -lt 5 )
-{
-  try 
-  {
-    Invoke-Command -Authentication CredSSP -ScriptBlock {New-AdOrganizationalUnit -Name "Domain Computers" -Path "DC=TFI,DC=LOCAL" -ProtectedFromAccidentalDeletion $False} -ComputerName $dcname -Credential $credential -ErrorAction Continue
-    
-    if ($Error[0] -match "An attempt was made to add an object to the directory with a name that is already in use")
-        {
-            Write-Host "Error caught in the IF statement: $($Error[0])"
-            Write-Host "Me thinks thy OU doth exist."
-            $Error[0] = $null
-            $status = 5
-        }
-
-    Write-Host "End of inital Try, total attempts: $($status)"
-    Write-Host "Starting Sleep for 60 seconds to try again."
-    Start-Sleep -s 60
-    $status += 1
-    }
-
-  catch
-  {
-   Write-Host "Starting Sleep for 60 seconds and trying again."
-   Start-Sleep -s 60
-   Write-Host "End catch attempt: $($status)"
-   $status += 1
-  }
-}
+Invoke-Command -Authentication CredSSP -ScriptBlock {choco install sql-server-express -y} -ComputerName $dcname -Credential $credential
+Invoke-Command -Authentication CredSSP -ScriptBlock {choco install sql-server-management-studio -y} -ComputerName $dcname -Credential $credential
 
 Stop-Transcript
